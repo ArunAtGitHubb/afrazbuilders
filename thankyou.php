c9235b9bb55acfb803a9b1578528cc00aff1b19c
Arun <arunislordofgod49@gmail.com>
2023-09-28 18:29:03 +0530

    Fix: thankyou.php

--- thankyou.php
+++ thankyou.php
@@ -15,32 +15,6 @@
 </header>
 
 
-<!-- Title Bar -->
-<div class="pbmit-title-bar-wrapper">
-			<div class="container">
-				<div class="pbmit-title-bar-content">
-					<div class="pbmit-title-bar-content-inner">
-						<div class="pbmit-tbar">
-							<div class="pbmit-tbar-inner container">
-								<h1 class="pbmit-tbar-title">Thank You</h1>
-							</div>
-						</div>
-						<div class="pbmit-breadcrumb">
-							<div class="pbmit-breadcrumb-inner">
-								<span><a title="" href="#" class="home"><span>Home</span></a></span>
-								<span class="sep">
-									<i class="pbmit-base-icon-angle-double-right"></i>
-								</span>
-								<span><span class="post-root post post-post current-item">Thank You</span></span>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- Title Bar End-->
-
-
     <section class="error-section" style="margin:50px; padding-bottom: 3rem;">
         <div class="container clearfix">
             <div class="error-text text-center">
